e46899836c569f71c758d618533582f9f1ff173e
Asmaa Kaddouri <Asmaa.Kaddouri@Student.HTW-Berlin.de>
2023-05-11 23:42:44 +0200

    Interface: InputParser Klasse: SimpleInputParser.java

--- app/src/main/java/htw/berlin/wi/prog2/service/SimpleInputParser.java
+++ app/src/main/java/htw/berlin/wi/prog2/service/SimpleInputParser.java
@@ -8,7 +8,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class SimpleInputParser {
+public class SimpleInputParser implements InputParser{
+    SimpleInputParser parser = new SimpleInputParser();
+
     public List<Ingredient> ingredientsFromInput(String inputLine) {
         List<Ingredient> result = new ArrayList<>();
         for (Ingredient ing : Menu.getAllArticles().values()) {
@@ -16,4 +18,5 @@ public class SimpleInputParser {
         }
         return result;
     }
+
 }
